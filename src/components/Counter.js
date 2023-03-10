06cc2cd0e7eb6a29b48e9c400f18ef49cb9664d0
Habtetare <121849266+Habtetare@users.noreply.github.com>
2023-03-10 15:08:08 +0300

    commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,25 +6,25 @@ class Counter extends Component{
         this.state={
             counter: 10,
         }; 
-            this.increment = this.increment.bind(this);
-            this.decriment = this.decriment.bind(this);
+            this.increment = this.increment.bind(this)
+            this.decriment = this.decriment.bind(this)
          
     }
     increment(){
         this.setState({
             counter: this.state.counter + 1,
-        })
+        });
     }
     decriment(){
         this.setState({
             counter: this.state.counter - 1,
-        })
+        });
     }
     render(){
         return (<div>
                 <h3> Count is :{this.state.counter}</h3>
-                <button onClick={this.increment()}>Incriment</button>
-                <button onClick={ this.decriment()}>Decriment</button>
+                <button onClick={this.increment}>Incriment</button>
+                <button onClick={ this.decriment}>Decriment</button>
             </div>
         );
     }
