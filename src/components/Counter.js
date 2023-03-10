7ea0693a51e10acf661c66b645f208e6129dc268
Habtetare <121849266+Habtetare@users.noreply.github.com>
2023-03-10 15:14:42 +0300

    commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,26 @@
 import { Component } from "react";
-
+// three way of binding event
 class Counter extends Component{
     constructor(){
         super();
         this.state={
             counter: 10,
         }; 
-            this.increment = this.increment.bind(this)
+
             this.decriment = this.decriment.bind(this)
          
     }
-    increment(){
+    increment =()=>{
+        console.log("incriment")
         this.setState({
             counter: this.state.counter + 1,
-        });
+        })
+    }
+    increment2 (){
+        console.log("incriment")
+        this.setState({
+            counter: this.state.counter + 2,
+        })
     }
     decriment(){
         this.setState({
@@ -25,6 +32,7 @@ class Counter extends Component{
                 <h3> Count is :{this.state.counter}</h3>
                 <button onClick={this.increment}>Incriment</button>
                 <button onClick={ this.decriment}>Decriment</button>
+                <button onClick={() => this.increment2()}>increment2</button>
             </div>
         );
     }
